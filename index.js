5716eb882209cd6344b9689d77481d94a92b7701
Tenzin Jorden <tenden96@gmail.com>
2021-12-03 15:01:55 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
-
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -19,8 +18,8 @@ function moveDodgerLeft() {
   });
 
   function moveDodgerRight() {
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(rightNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
   
     if (left < 360) {
       dodger.style.left = `${left + 1}px`;
